ac935b27607c32b3e2e791fe40cd5b9750b5b735
Idab Benabu <idabbenabu@Idabs-MacBook-Pro.local>
2024-03-03 00:51:36 +0200

    call user to save it in localstorage user key

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import "./App.css";
+import { useEffect } from "react";
+import { HashRouter as Router } from "react-router-dom";
 
-import { HashRouter as Router } from 'react-router-dom';
 import { createRouting, routes } from "./routes";
+import { userService } from "./services/user.service";
 
 function App() {
-
-  return (
-    <Router>
-      {createRouting(routes)}
-    </Router>
-  );
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        await userService.query();
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    loadUser();
+  }, []);
+  return <Router>{createRouting(routes)}</Router>;
 }
 
 export default App;
