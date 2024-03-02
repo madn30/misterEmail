0190596522c82e0b01e6176fb66ae97fa80cbf51
Idab Benabu <idabbenabu@Idabs-MacBook-Pro.local>
2024-03-03 00:49:32 +0200

    wrap the paper in main tag for symentics

--- src/components/Paper/Paper.jsx
+++ src/components/Paper/Paper.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
 export default function Paper({ children, className }) {
-  console.log(className)
-  return <div className={`paper ${className}`}>{children}</div>;
+  return <main className={`paper ${className}`}>{children}</main>;
 }
