a0266edd11b346e595f5cd972cca81f877588e05
Idab Benabu <idabbenabu@Idabs-MacBook-Pro.local>
2024-03-03 00:52:59 +0200

    add content for tooltip content

--- src/layouts/MainLayout/NavBar/index.jsx
+++ src/layouts/MainLayout/NavBar/index.jsx
@@ -1,27 +1,26 @@
 import React from "react";
-import { MdOutlineInbox } from "react-icons/md";
-import { FaRegStar } from "react-icons/fa";
-import { RiDraftLine } from "react-icons/ri";
-import { VscSend } from "react-icons/vsc";
+import { MdOutlineInbox as InboxIcon} from "react-icons/md";
+import { FaRegStar as StarIcon } from "react-icons/fa";
+import { RiDraftLine as DraftIcon} from "react-icons/ri";
+import { VscSend as SendIcon} from "react-icons/vsc";
 import NavItem from "./NavItem";
-import Compose from "../../../components/Compose/Compose";
+import SendMail from "../../../components/SendMail/SendMail";
 import ToolTip from "../../../components/ToolTip/ToolTip";
 
 export default function NavBar() {
   const sections = [
-    { title: "Inbox", icon: MdOutlineInbox },
-    { title: "Starred", icon: FaRegStar },
-    { title: "Sent", icon: VscSend },
-    { title: "Draft", icon: RiDraftLine },
-    // { title: "Trash", icon: FaRegTrashCan },
+    { title: "Inbox", icon: InboxIcon },
+    { title: "Starred", icon: StarIcon },
+    { title: "Sent", icon: SendIcon },
+    { title: "Draft", icon: DraftIcon },
   ];
 
   return (
     <nav className="nav-bar">
-      <Compose />
+      <SendMail />
       {sections.map((section, index) => (
-        <ToolTip position="right">
-          <NavItem key={index} title={section.title} icon={section.icon} />
+        <ToolTip position="right" key={index} content={section.title}>
+          <NavItem  title={section.title} icon={section.icon} />
         </ToolTip>
       ))}
     </nav>
