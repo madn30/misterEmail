f02d28c0845e673021f4ddfd7f7bf1e2253c7435
Idab Benabu <idabbenabu@Idabs-MacBook-Pro.local>
2024-03-04 15:08:20 +0200

    recieve props onClick to handle logic in the parent component

--- src/components/IconButton/IconButton.jsx
+++ src/components/IconButton/IconButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function IconButton({ children, onClick, className }) {
-  return <div className={className} onClick={onClick}>{children}</div>;
+export default function IconButton({ children, onClick }) {
+  return <div className="icon-button" onClick={onClick}>{children}</div>;
 }
