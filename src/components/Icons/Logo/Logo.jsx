c3481efa4db2e0c405497588910abb29713fbc52
Idab Benabu <idabbenabu@Idabs-MacBook-Pro.local>
2024-03-05 16:58:27 +0200

    suppoprt logo with Link to property

--- src/components/Icons/Logo/Logo.jsx
+++ src/components/Icons/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Logo() {
   return (
-    <Link href='/inbox'>
+    <Link to='/'>
     <img src='https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png'/>
     </Link>
   )
