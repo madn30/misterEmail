1873e28a961bd0842130155c93222f8806f0a192
Idab Benabu <idabbenabu@Idabs-MacBook-Pro.local>
2024-03-09 01:53:05 +0200

    add Outlet from react-router-dom to display the content

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import MainHeader from "../../components/MainHeader/MainHeader";
 import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
 
-export const MainLayout = ({ children }) => (
+const MainLayout = ({ children }) => (
   <div className="main-layout-grid">
     <MainHeader />
     <NavBar />
-    <div className="content-container">{children}</div>
+    {/* <div className="content-container">{children}</div> */}
+    <Outlet/>
   </div>
 );
+export default  MainLayout
