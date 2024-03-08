d30dc5e041fd0235b9bde48f6760ef6df570425a
Idab Benabu <idabbenabu@Idabs-MacBook-Pro.local>
2024-03-09 01:49:45 +0200

    add unread number span

--- src/layouts/MainLayout/NavBar/NavItem.jsx
+++ src/layouts/MainLayout/NavBar/NavItem.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { emailService } from "../../../services/email.service";
 
 export default function NavItem({ navItem }) {
   const Icon = navItem.icon;
+  const count = emailService.countUnreadEmailsInFolder(navItem.title);
   return (
-    <NavLink to={navItem.to} className="nav-item">
+    <NavLink to={`${navItem.to}`} className="nav-item">
       <Icon />
       {navItem.title}
+      <span className="unread-emails">{count > 0 ? count : ""}</span>
     </NavLink>
   );
 }
