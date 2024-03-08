b54f50f277fb0a818314d8934a173589a87f8da4
Idab Benabu <idabbenabu@Idabs-MacBook-Pro.local>
2024-03-09 01:46:27 +0200

    add libary for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path'; // Corrected import statement
 
 export default defineConfig({
+  base: "/misterEmail/",
   plugins: [react()],
- 
+
 });
