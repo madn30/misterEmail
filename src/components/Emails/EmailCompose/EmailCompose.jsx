b0f0461a900ffd97035529ce6cbd89fa1e8577d2
Idab Benabu <idabbenabu@Idabs-MacBook-Pro.local>
2024-03-09 20:34:50 +0200

    add advanced filter styles and logic to smart filters the emails

--- src/components/Emails/EmailCompose/EmailCompose.jsx
+++ src/components/Emails/EmailCompose/EmailCompose.jsx
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
-import { MdClose as CloseIcon } from "react-icons/md";
+
 import { emailService } from "../../../services/email.service";
-import { useNavigate, useOutletContext } from "react-router";
-import { Link } from "react-router-dom";
 import ComposeIcon from "../../Icons/ComposeIcon/ComposeIcon";
 import { eventBusService } from "../../../services/event-bus.service";
-import { userService } from "../../../services/user.service";
+
+import { MdClose as CloseIcon } from "react-icons/md";
 
 export default function EmailCompose() {
   const [email, setEmail] = useState({ to: "", subject: "", body: "" });
   const [isOpen, setIsOpen] = useState(false);
-  const context = useOutletContext();
-  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -28,11 +25,15 @@ export default function EmailCompose() {
     const payload = {
       ...defaultEmail,
       ...email,
-      folder: ["Sent"],
+      folder: ["sent"],
     };
     try {
-      // context.onAddEmail(payload);
       eventBusService.emit("compose-form", payload);
+      eventBusService.emit("show-message", {
+        message: "Message sent.",
+      });
+      setIsOpen(false);
+
     } catch (err) {
       console.error(err);
     }
