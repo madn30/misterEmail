6dd1caf004d27c4d3509bcd25c6da99898999f7b
Idab Benabu <idabbenabu@Idabs-MacBook-Pro.local>
2024-03-10 00:13:37 +0200

    push the composeButton query Link tag

--- src/components/Icons/ComposeIcon/ComposeIcon.jsx
+++ src/components/Icons/ComposeIcon/ComposeIcon.jsx
@@ -1,11 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { MdOutlineModeEditOutline as EditIcon } from "react-icons/md";
 
-export default function ComposeIcon({onClick}) {
+export default function ComposeIcon() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('?compose=new');
+  };
+
   return (
-    <button className="flex align-center compose-button" onClick={onClick}>
+    <div className="flex align-center compose-button" onClick={handleClick}>
       <EditIcon /> Compose
-    </button>
+    </div>
   );
 }
