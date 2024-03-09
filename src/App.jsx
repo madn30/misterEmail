a28765b0c78ba48c5d40584abc44340025d1fbc4
Idab Benabu <idabbenabu@Idabs-MacBook-Pro.local>
2024-03-10 00:10:29 +0200

    remove comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import { useEffect } from "react";
-import {
-  Navigate,
-  Route,
-  HashRouter as Router,
-  Routes,
-  useParams,
-} from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
-// import { createRouting, routes } from "./routes";
+import { HashRouter as Router } from "react-router-dom";
 import { userService } from "./services/user.service";
 import { Snackbar } from "./components/SnackBar/SnackBar";
-import EmailIndex from "./pages/EmailIndex/EmailIndex";
-import EmailCompose from "./components/Emails/EmailCompose/EmailCompose";
-import EmailDetails from "./pages/EmailDetails/EmailDetails";
 import { createRouting, routes } from "./routes";
 
 function App() {
+
   useEffect(() => {
     const loadUser = async () => {
       try {
@@ -29,9 +19,7 @@ function App() {
 
   return (
     <>
-      <Router basename="/">
-        {createRouting(routes)}
-      </Router>
+      <Router basename="/">{createRouting(routes)}</Router>
       <Snackbar />
     </>
   );
