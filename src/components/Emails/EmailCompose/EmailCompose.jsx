6990e319f5d9df5d47df1b91d4a38fcf4c67a861
Idab Benabu <idabbenabu@Idabs-MacBook-Pro.local>
2024-03-10 00:12:14 +0200

    remove navigate(-1) change it with query

--- src/components/Emails/EmailCompose/EmailCompose.jsx
+++ src/components/Emails/EmailCompose/EmailCompose.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
-
 import { emailService } from "../../../services/email.service";
-import ComposeIcon from "../../Icons/ComposeIcon/ComposeIcon";
-import { eventBusService } from "../../../services/event-bus.service";
-
 import { MdClose as CloseIcon } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+import { eventBusService } from "../../../services/event-bus.service";
 
 export default function EmailCompose() {
   const [email, setEmail] = useState({ to: "", subject: "", body: "" });
-  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -29,62 +27,56 @@ export default function EmailCompose() {
     };
     try {
       eventBusService.emit("compose-form", payload);
-      eventBusService.emit("show-message", {
-        message: "Message sent.",
-      });
-      setIsOpen(false);
-
     } catch (err) {
       console.error(err);
     }
   };
-  const toggleComposeModal = () => {
-    setIsOpen((prevIsOpen) => !prevIsOpen);
+
+  const handleClose = () => {
+    navigate(-1);
   };
+
   return (
     <div className="nav-bar-compose-root">
-      <ComposeIcon onClick={toggleComposeModal} />
-      {isOpen && (
-        <form className="send-mail-modal" onSubmit={handleSubmit}>
-          <header>
-            New message
-            <button
-              type="button"
-              onClick={toggleComposeModal}
-              className="close-button"
-            >
-              <CloseIcon />
-            </button>
-          </header>
-          <section>
-            <input
-              name="to"
-              placeholder="To"
-              value={email.to}
-              onChange={handleChange}
-            />
-            <input
-              name="subject"
-              placeholder="Subject"
-              value={email.subject}
-              onChange={handleChange}
-            />
-            <textarea
-              name="body"
-              rows="10"
-              maxLength="2000"
-              placeholder="Your message here"
-              value={email.message}
-              onChange={handleChange}
-            />
-          </section>
-          <footer>
-            <button className="send-button" type="submit">
-              Send
-            </button>
-          </footer>
-        </form>
-      )}
+      <form className="send-mail-modal" onSubmit={handleSubmit}>
+        <header>
+          New message
+          <button
+            type="button"
+            onClick={handleClose}
+            className="close-button"
+          >
+            <CloseIcon />
+          </button>
+        </header>
+        <section>
+          <input
+            name="to"
+            placeholder="To"
+            value={email.to}
+            onChange={handleChange}
+          />
+          <input
+            name="subject"
+            placeholder="Subject"
+            value={email.subject}
+            onChange={handleChange}
+          />
+          <textarea
+            name="body"
+            rows="10"
+            maxLength="2000"
+            placeholder="Your message here"
+            value={email.body} 
+            onChange={handleChange}
+          />
+        </section>
+        <footer>
+          <button className="send-button" type="submit">
+            Send
+          </button>
+        </footer>
+      </form>
     </div>
   );
 }
